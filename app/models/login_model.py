baa058a424c844cd244a158130e77f398386fab4
Mauricio <mauriciodg00@gmail.com>
2022-06-16 11:00:05 -0500

    #12 login model registerUser versión funcional inicial

---
+++ app/models/login_model.py
@@ -0,0 +1,29 @@
+from hashlib import new
+from pickle import TRUE
+from app import app
+from app.classes import User
+from flask import Flask, session
+from app import app
+class login_model:
+    def loginUser(self, username, password):
+        user=app.firebaseManager.loginMedico(username, password)
+        if user:
+            app.loggedUser=user
+            self.startSession(user)
+            return True
+        else:
+            return False
+    def registerUser(nombre,correo, password, confirmaPassword):
+        if password!=confirmaPassword:
+            return False
+        user=app.firebaseManager.registraUsuario(nombre, correo, password)
+        if user:
+            return True
+        else:
+            return False
+
+    def startSession(self, user):
+        session["id"]=user.id
+        session["nombre"]=user.nombreCompleto
+        session["correo"]=user.correo
+
