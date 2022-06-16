950d0d1094f273b0a88fd229312771735e0025e2
DANIELA GOMEZ PENICHE <danygp1999@gmail.com>
2022-06-16 12:06:13 -0500

    #7 progreso

--- java.js
+++ java.js
@@ -9,23 +9,20 @@ var tweet = ["hola", "hola", "hola", "hola", "hola", "hola", "hola", "hola", "ho
              "hola", "hola", "hola", "hola", "hola", "hola", "hola", "hola", "hola", "hola final"];
 
 
-var post = "<h6 class=\"text-body\">";
-</h6><span class="small text-muted font-weight-normal"> • </span>
-                      <span class="small text-muted font-weight-normal" id="TimeStamp"></span>
-                      <span><i class="fas fa-angle-down float-end"></i></span>
-                      <span class="small text-muted font-weight-normal" id="UserName"></span>
-                      <p style="line-height: 1.2;" id="Tweet"></p>
 //Función que imporime los tweets
 window.onload = function() {
     for(i=0;i<10;i++) {
-        const div0 = document.getElementById('ScreenName');
-        div0.insertAdjacentHTML('beforeend', screenname[i]);
-        const div1 = document.getElementById('UserName');
-        div1.insertAdjacentHTML('beforeend', username[i]);
-        const div2 = document.getElementById('TimeStamp');
-        div2.insertAdjacentHTML('beforeend', timestamp[i]);
-        const div3 = document.getElementById('Tweet');
-        div3.insertAdjacentHTML('beforeend', tweet[i]);
+        var post = "<div><h6 class=\"text-body\">";
+        post += screenname[i];
+        post += "</h6><span class=\"small text-muted font-weight-normal\"> • </span><span class=\"small text-muted font-weight-normal\">";
+        post += username[i];
+        post += "</span><span><i class=\"fas fa-angle-down float-end\"></i></span><span class=\"small text-muted font-weight-normal\">";
+        post += timestamp[i];
+        post += "</span><p style=\"line-height: 1.2;\">";
+        post += tweet[i];
+        post += "</p></div>";
+        const div = document.getElementById('divprint');
+        div.insertAdjacentHTML('beforeend', post);
     }
 }
 
