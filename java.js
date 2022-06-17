2220cb146cc0d37adcc1226a6d576f7e54ea90f7
Joshua Haase <hahj87@gmail.com>
2022-06-17 12:36:04 -0500

    #7 fix linter complaints.

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 //JSON temporales en lo que queda la base de datos
-var Informacion= { 
+var Informacion= {
     "TweetsRecibidos" : [
         {"screenname":"Miley Cyrus", "username":"@hannamontana", "timestamp":"2h", "tweet":"hola"},
         {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
@@ -81,7 +81,7 @@ function ImprimeTweets(number) {
             // Desactivar el botón si no hay más tweets por cargar
             else {
                 document.getElementById("cargar").disabled = true;
-                document.getElementById("cargar").innerHTML = "Esperando tweets"; 
+                document.getElementById("cargar").innerHTML = "Esperando tweets";
             }
         }
     }
@@ -94,7 +94,7 @@ $(window).on("scroll", function() {
     var scrollHeight = $(document).height();
     //Posición del cursor
     var scrollPos = $(window).height() + $(window).scrollTop();
-    // Disparar si la posición del cursor está 300 pixeles más abajo del principio de la pagina 
+    // Disparar si la posición del cursor está 300 pixeles más abajo del principio de la pagina
     if(((scrollHeight - 300) >= scrollPos) / scrollHeight == 0){
         number = number+10;
         $("#cargar").click();
