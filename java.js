5c9f91e2d75cb723a1c470a51d2e2b0026c61165
DANIELA GOMEZ PENICHE <danygp1999@gmail.com>
2022-06-17 11:32:42 -0500

    #7 pagina usando JSONS

--- java.js
+++ java.js
@@ -1,39 +1,34 @@
 //JSON temporales en lo que queda la base de datos
-var TweetsRecibidos = [
-    {"screenname":"Miley Cyrus", "username":"@hannamontana", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
-    {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"}
-]
+var Informacion= { 
+    "TweetsRecibidos" : [
+        {"screenname":"Miley Cyrus", "username":"@hannamontana", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola"},
+        {"screenname":"Miley Cyrus", "username":"@myleycyrus", "timestamp":"2h", "tweet":"hola final"}
+    ],
+    "numTweets" : 21,
+    "usuario" : "@hannamontana"
+}
 
-//Arreglos temporales en lo que queda la base de datos
-var screenname = ["Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus",
-                  "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus", "Miley Cyrus"];
-var username = ["@hannamontana", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus",
-                "@hannamontana", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus", "@mileycyrus final"];
-var timestamp = ["2h", "2h", "2h", "2h", "2h", "2h", "2h", "2h", "2h", "2h",
-                 "2h", "2h", "2h", "2h", "2h", "2h", "2h", "2h", "2h", "2h"];
-var tweet = ["hola", "hola", "hola", "hola", "hola", "hola", "hola", "hola", "hola", "hola",
-             "hola", "hola", "hola", "hola", "hola", "hola", "hola", "hola", "hola", "hola final"];
-
-var miUsuario = "@hannamontana";
+miUsuario = Informacion.usuario;
+numeroTweets = Informacion.numTweets-1;
 
 number=0; //Variable que permite cargar los siguientes 10 tweets
 
@@ -48,20 +43,20 @@ const sleep = async (milliseconds) => {
 window.onload = function() {
     for(i=0;i<10;i++) {
         // Validar que siga habiendo tweets
-        if (TweetsRecibidos[i].screenname !== null) {
+        if (i<=numeroTweets) {
             //En post se guardaa el HTML para el tweet, agregandole la información de la base de datos
             var post = "<h6 class=\"text-body\">";
             post += "<div class=\"p-3 border-bottom\">";
             post += "<img src=\"https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (29).webp\" class=\"rounded-circle\" height=\"50\" loading=\"lazy\"/>  ";
-            post += TweetsRecibidos[i].screenname;
+            post += Informacion.TweetsRecibidos[i].screenname;
             post += "<div style=\"display:inline\" class=\"small text-muted font-weight-normal\"> • </div>";
             post += "<span class=\"small text-muted font-weight-normal\">";
-            post += TweetsRecibidos[i].username;
+            post += Informacion.TweetsRecibidos[i].username;
             post += "</span><span class=\"small text-muted font-weight-normal\">  ";
-            post += TweetsRecibidos[i].timestamp;
+            post += Informacion.TweetsRecibidos[i].timestamp;
             post += "</span>";
             //Menu para borrar, editar, etc., tus propios tweets
-            if(TweetsRecibidos[i].username==miUsuario){
+            if(Informacion.TweetsRecibidos[i].username==miUsuario){
                 post += "<div class=\"dropdown\" style=\"display: inline-block\">";
                 post += "<button class=\"btn btn-link dropdown-toggle\" type=\"button\" id=\"dropdownMenu\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\" style=\"color: grey\"></button>";
                 post += "<ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuButton1\">";
@@ -70,7 +65,7 @@ window.onload = function() {
                 post += "</ul></div>";
             }
             post += "</div></h6><p class=\"p-3 border-bottom border-dark\" style=\"line-height: 1.2;\">";
-            post += TweetsRecibidos[i].tweet;
+            post += Informacion.TweetsRecibidos[i].tweet;
             post += "</p>";
             //El contenido de post se agrega como hijo de un div vacío
             const div = document.getElementById('divprint');
@@ -86,21 +81,20 @@ function ImprimeTweets(number) {
         await sleep(1000);
         for(i=number;(i<(number+10));i++) {
             // Validar que siga habiendo tweets
-            console.log(TweetsRecibidos[i].screenname);
-            if (!(jQuery.isEmptyObject(TweetsRecibidos[i].screenname))) {
+            if (i<=numeroTweets) {
                 //En post se guardaa el HTML para el tweet, agregandole la información de la base de datos
                 var post = "<h6 class=\"text-body\">";
                 post += "<div class=\"p-3 border-bottom\">";
                 post += "<img src=\"https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (29).webp\" class=\"rounded-circle\" height=\"50\" loading=\"lazy\"/>  ";
-                post += TweetsRecibidos[i].screenname;
+                post += Informacion.TweetsRecibidos[i].screenname;
                 post += "<div style=\"display:inline\" class=\"small text-muted font-weight-normal\"> • </div>";
                 post += "<span class=\"small text-muted font-weight-normal\">";
-                post += TweetsRecibidos[i].username;
+                post += Informacion.TweetsRecibidos[i].username;
                 post += "</span><span class=\"small text-muted font-weight-normal\">  ";
-                post += TweetsRecibidos[i].timestamp;
+                post += Informacion.TweetsRecibidos[i].timestamp;
                 post += "</span>";
                 //Menu para borrar, editar, etc., tus propios tweets
-                if(TweetsRecibidos[i].username==miUsuario){
+                if(Informacion.TweetsRecibidos[i].username==miUsuario){
                     post += "<div class=\"dropdown\" style=\"display: inline-block\">";
                     post += "<button class=\"btn btn-link dropdown-toggle\" type=\"button\" id=\"dropdownMenu\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\" style=\"color: grey\"></button>";
                     post += "<ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuButton1\">";
@@ -109,7 +103,7 @@ function ImprimeTweets(number) {
                     post += "</ul></div>";
                 }
                 post += "</div></h6><p class=\"p-3 border-bottom border-dark\" style=\"line-height: 1.2;\">";
-                post += TweetsRecibidos[i].tweet;
+                post += Informacion.TweetsRecibidos[i].tweet;
                 post += "</p>";
                 //El contenido de post se agrega como hijo de un div vacío
                 const div = document.getElementById('divprint');
