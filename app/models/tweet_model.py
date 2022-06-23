d328aedbfbc84853161b36b871f498aa6e103e03
ramon-san <ramon.orraca@correo.uia.mx>
2022-06-23 10:02:09 -0500

    #31 fix Se elimina el acceso predeterminado.

--- app/models/tweet_model.py
+++ app/models/tweet_model.py
@@ -8,9 +8,9 @@ from app import app
 class tweet_model:
     def tweetMetadata(self):
         metadata = {
-            "userID": session.get("id") or "nqJOCtVKpSMMVyyBg6bHghbEIoa2",
-            "username": session.get("username") or "joshua.haase",
-            "name": session.get("name") or "Joshua",
+            "userID": session.get("id"),
+            "username": session.get("username"),
+            "name": session.get("name"),
         }
         print(metadata["userID"])
         if metadata["userID"] is None:
